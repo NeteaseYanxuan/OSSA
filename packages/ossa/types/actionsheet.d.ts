6f9d73421e987bb59564390c2c447ed6a4bcfb5b
tangwenhui <tangwenhui@rd.netease.com>
2022-10-27 08:10:25 +0800

    feat: 完善类型文件注释

--- packages/ossa/types/actionsheet.d.ts
+++ packages/ossa/types/actionsheet.d.ts
@@ -2,16 +2,46 @@ import { ComponentClass, ReactChild } from "react";
 import OsComponent from "./base";
 import { OsCommonFunction } from "./common";
 
+/**
+ * @name 动作面板 
+ * @description 从底部弹出的动作菜单面板。 
+ * @see https://ossa.miaode.com/docs/%E7%BB%84%E4%BB%B6/actionsheet
+ */
 interface ActionsheetProps extends OsComponent {
-  title?: String;
-  isShow: Boolean;
-  cancelText?: String;
+  /**
+   * @prop 面板标题
+   */
+  title?: string;
+  /**
+   * @prop 是否显示弹窗
+   */
+  isShow: boolean;
+  /**
+   * @prop 取消按钮文本
+   */
+  cancelText?: string;
+  /**
+   * @function onClose 关闭面板回调
+   */
   onClose?: OsCommonFunction;
 }
 
+/**
+ * @name 操作面板数据项
+ * @description 在操作面板的每一个数据项的结构
+ */
 interface ActionsheetItemProps extends OsComponent {
-  title: String;
-  disabled?: Boolean;
+  /**
+   * @prop 面板数据项标题
+   */
+  title: string;
+  /**
+   * @prop 该数据项是否禁止操作
+   */
+  disabled?: boolean;
+  /**
+   * @function onClose 关闭面板回调
+   */
   onClick?: OsCommonFunction;
 }
 
--- packages/ossa/types/animate.d.ts
+++ packages/ossa/types/animate.d.ts
@@ -2,6 +2,10 @@ import { ComponentClass, ReactChild } from "react";
 import { CommonEventFunction } from "@tarojs/components/types/common";
 import OsComponent from "./base";
 
+/**
+ * @name 动画
+ * @description 常见的过场动画
+ */
 interface AnimateProps extends OsComponent {
   type?: string;
   onClick?: CommonEventFunction;
