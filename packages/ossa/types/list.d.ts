5fec612255bd9830d63e57fe314fdac48e80ad5f
tangwenhui <tangwenhui@rd.netease.com>
2022-11-05 20:31:38 +0800

    feat: 列表类型描述

--- packages/ossa/types/list.d.ts
+++ packages/ossa/types/list.d.ts
@@ -23,17 +23,53 @@ interface ListProps extends OsComponent {
    * 副标题
    */
   subTitle?: string;
-  
+  /**
+   * 右侧图标
+   * @see https://ossa.miaode.com/docs/%E7%BB%84%E4%BB%B6/icon
+   */
   rightIcon?: IconProps["type"];
+  /**
+   * 左侧图标
+   * @see https://ossa.miaode.com/docs/%E7%BB%84%E4%BB%B6/icon
+   */
   leftIcon?: IconProps["type"];
+  /**
+   * 简要描述
+   */
   desc?: string;
-  active?: string;
+  /**
+   * 是否展示分割线
+   * @default true
+   */
   showSplitLine?: boolean;
+  /**
+   * 标题颜色
+   * @default #333333
+   */
   color?: string;
+  /**
+   * 背景色
+   * @default #ffffff
+   */
   bgColor?: string;
+  /**
+   * 激活态背景色
+   * @default #f9f9f9
+   */
   activeBgColor?: string;
+  /**
+   * 左侧图标尺寸
+   * @default 36
+   */
   leftIconSize?: number;
+  /**
+   * 副标题或者描述字体颜色
+   * @default #7f7f7f
+   */
   descColor?: string;
+  /**
+   * 点击回调
+   */
   onClick?: CommonEventFunction;
 }
 
