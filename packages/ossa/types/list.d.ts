fb5b544cc0e9deeb18d99ce13e97cc7195750176
tangwenhui <tangwenhui@rd.netease.com>
2022-11-05 20:16:08 +0800

    feat: 列表类型描述

--- packages/ossa/types/list.d.ts
+++ packages/ossa/types/list.d.ts
@@ -3,9 +3,25 @@ import { CommonEventFunction } from "@tarojs/components/types/common";
 import OsComponent from "./base";
 import { IconProps } from "./icon";
 
+/**
+ * @name 通用列表展示。
+ */
 interface ListProps extends OsComponent {
+  /**
+   * 展示类型
+   * @default "default"
+   * @desc
+   * - "default": 默认列表展示形式
+   * - "custom": 自定义列表样式
+   */
+  type?: "default" | "custom";
+  /**
+   * 主标题
+   */
   title?: string;
-  type?: string;
+  /**
+   * 副标题
+   */
   subTitle?: string;
   rightIcon?: IconProps["type"];
   leftIcon?: IconProps["type"];
