536ded921d113d5f21338c565b00b5a033f997a4
tangwenhui <tangwenhui@rd.netease.com>
2022-11-06 09:47:12 +0800

    feat: 导航栏类型描述优化

--- packages/ossa/types/navbar.d.ts
+++ packages/ossa/types/navbar.d.ts
@@ -7,8 +7,23 @@ export interface NavBarItemProps extends OsComponent {
   text: string;
 }
 export interface NavBarProps extends OsComponent {
-  title?: String;
-  type?: String;
+  /**
+   * 导航标题
+   */
+  title?: string;
+  /**
+   * 展示类型
+   * @desc
+   * - "2column": 两列均分
+   * - "3column": 三列均分
+   * - "custom": 自定义
+   * @default "3column"
+   */
+  type?: "2column" | "3column" | "custom";
+  /**
+   * icon的颜色，仅当使用ossa提供的icon时有效
+   * @default "#7f7f7f"
+   */
   iconColor?: string;
   middleSlot?: ReactChild;
   leftIcons?: Array<IconProps["type"]>;
