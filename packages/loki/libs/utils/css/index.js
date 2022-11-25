83ae0e4aacd402847d75f1e69b4360b5693145c6
xuyang03 <xuyang03@corp.netease.com>
2022-11-25 17:56:27 +0800

    fix: 优化px2rem

--- packages/loki/libs/utils/css/index.js
+++ packages/loki/libs/utils/css/index.js
@@ -8,12 +8,17 @@ export const px2rem = function ($px) {
   ) {
     return $px + "rpx";
   }
-  if (typeof process.env.TARO_ENV !== "undefined" && process.env.TARO_ENV) {
-    return $px + "rpx";
+  try {
+    if (typeof process.env.TARO_ENV !== "undefined" && process.env.TARO_ENV) {
+      return $px + "rpx";
+    }
+  } catch (error) {
+    baseSize = (
+      window.getComputedStyle(document.getElementsByTagName("html")[0])[
+        "font-size"
+      ] || window.getComputedStyle(document.body)["font-size"]
+    ).replace(/[a-zA-Z]+$/, "");
   }
-  baseSize = window
-    .getComputedStyle(document.getElementsByTagName("html")[0])
-    ["font-size"].replace(/[a-zA-Z]+$/, "");
   // @ts-ignore
   return `${$px / baseSize}rem`;
 };
