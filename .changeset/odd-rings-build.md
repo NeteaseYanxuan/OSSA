01fb169413eea7194d1e9ce164187ecb9549bba1
kiner-tang(文辉) <1127031143@qq.com>
2022-11-30 20:41:16 +0800

    fix: 修改部分 stepper 组件错误 与完善类型描述

---
+++ .changeset/odd-rings-build.md
@@ -0,0 +1,7 @@
+---
+"ossaui": patch
+"ossa-demo": patch
+"ossa-doc": patch
+---
+
+fix: 修改部分 stepper 组件错误 与完善类型描述
--- packages/ossa/types/stepper.d.ts
+++ packages/ossa/types/stepper.d.ts
@@ -1,6 +1,23 @@
 import { ComponentClass, ReactChild } from "react";
 import OsComponent from "./base";
 
+
+export type InputErrorParam = {
+  /**
+   * 错误类型
+   * @desc
+   * - DISABLED: 步进器处于禁用状态时触发
+   * - LOW: 操作后小于最小值时触发
+   * - OVER: 操作后大于最小值时触发
+   */
+  type: "DISABLED" | "LOW" | "OVER",
+  /**
+   * 矫正后的值
+   * @desc 如果输入值小于最小值则为最小值，大于最大值则为最大值
+   */
+  errorValue: number;
+};
+
 /**
  * @name 步进器
  */
@@ -42,13 +59,40 @@ interface StepperProps extends OsComponent {
    * @default 1
    */
   step?: number;
+  /**
+   * 当前步进器的值
+   */
   value: number;
+  /**
+   * 点击增加按钮回调
+   * @param value 最新值
+   */
   onAdd?: (value: number) => void;
+  /**
+   * 点击减少按钮回调
+   * @param value 最新值
+   */
   onSubtract?: (value: number) => void;
+  /**
+   * 当步进器改变时回调
+   */
   onChange: (value: number) => void;
-  onErrorInput?: (value: number) => void;
-  onBlur?: (event: any) => void;
+  /**
+   * 异常输入时回调
+   * @param info 异常输入的回调信息
+   */
+  onErrorInput?: (info: InputErrorParam) => void;
+  /**
+   * 失去焦点时回调
+   */
+  onBlur?: (event: BaseEventOrig<InputProps.inputValueEventDetail>) => void;
+  /**
+   * 禁止增加回调
+   */
   onDisabledAdd?: (value: number) => void;
+  /**
+   * 禁止减小回调
+   */
   onDisabledSubtract?: (value: number) => void;
 }
 
