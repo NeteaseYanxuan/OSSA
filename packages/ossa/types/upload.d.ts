56c33bde4035e4f263cc8949d29712fa3956c8b9
CaedmonW <107974245+CaedmonW@users.noreply.github.com>
2022-12-07 14:16:51 +0800

    Feature upload callback (#105)
    * feat(uplaod): add onImageClick
    
    * fix: import path
    
    * docs: onFail type definition
    
    * fix: cnpm package install failed
    
    * refactor: update typescript version
    
    * chore: lock @swc/core to 1.3.6

--- packages/ossa/types/upload.d.ts
+++ packages/ossa/types/upload.d.ts
@@ -2,21 +2,36 @@ import { ComponentClass, ReactChild } from "react";
 import { CommonEventFunction } from "@tarojs/components/types/common";
 import OsComponent from "./base";
 
+interface ImageFile {
+  /** 本地临时文件路径 */
+  path: string
+  /** 本地临时文件大小，单位 B */
+  size: number
+  /** 文件的 MIME 类型
+   * @supported h5
+   */
+  type?: string
+  /** 原始的浏览器 File 对象
+   * @supported h5
+   */
+  originalFileObj?: File
+}
+
 interface UploadProps extends OsComponent {
   max?: number;
   multiple?: boolean;
   files?: any;
-  customStyle?: object;
+  customStyle?: CSSProperties;
   classNames?: string;
   onChange?: (
-    files: Array<Object>,
+    files: Array<ImageFile>,
     operationType: string,
     index: number
   ) => void;
-  onImageClick?: (index: number, file: Object) => void;
-  onFail?: (message: string) => void;
+  onImageClick?: (index: number, file: ImageFile) => void;
+  onFail?: (err: TaroGeneral.CallbackResult) => void;
 }
 
 declare const Upload: ComponentClass<UploadProps>;
 
-export { Upload, UploadProps };
+export { Upload, UploadProps, ImageFile };
